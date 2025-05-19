a55364e75516fda7fe6c5bf729436da9fc6f5441
vignesh <vignesh@bijib.in>
2025-05-19 11:17:46 +0530

    .gitignore changes

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ require('./config/constants');
 const cors = require('cors');
 const app = express();
 const PORT = 3000;
+app.use(express.json());
+app.use(cors());
 
 connectDB().then(() => {
-  app.use(express.json());
-  app.use(cors());
   app.use("/", routes);
   app.listen(PORT, () => {
     console.log(`port running on ${PORT}`);
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,8 @@ const router = express.Router();
 const login = require('./login/index');
 
 router.use('/login', login);
+router.get('/health-check', (req, res) => {
+    res.status(200).json({ "status": true, "time": new Date() })
+})
 
 module.exports = router;
