3a2f08153526eff2d9e2dcd8805d9b98d79a1ea1
vignesh <vignesh@bijib.in>
2025-05-30 15:39:44 +0530

    changes

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -1,29 +1,29 @@
 const usersModel = require('../../models/users');
 
 exports.addUser = async (req, res) => {
-  try {
-    const params = req['body'];
-    await usersModel.addUser(params);
-    res.status(200).json({ "status": true, "msg": "Record added successfully" });
-  } catch (error) {
-    res.status(400).json({ "status": false, "error": [{"msg": error.msg}] });
-  }
+ try {
+  const params = req['body'];
+  const result = await usersModel.addUser(params);
+  res.status(200).json(result);
+ } catch (error) {
+  res.status(400).json({ "status": false, "error": [{ "msg": error.msg }] });
+ }
 }
 exports.getUser = async (req, res) => {
-  try {
-    const params = req['body'];
-    const result = await usersModel.getUser(params);
-    res.status(200).json({ "status": true, "data": result });
-  } catch (error) {
-    res.status(400).json({ "status": false, "error": [{ "msg": error.msg }] });
-  }
+ try {
+  const params = req['body'];
+  const result = await usersModel.getUser(params);
+  res.status(200).json({ "status": true, "data": result });
+ } catch (error) {
+  res.status(400).json({ "status": false, "error": [{ "msg": error.msg }] });
+ }
 }
 exports.comparePassword = async (req, res) => {
-  try {
-    const params = req['body'];
-    const result = await usersModel.comparePassword(params);
-    res.status(200).json(result);
-  } catch (error) {
-    res.status(400).json({ "status": false, "error": [{ "msg": error.msg }] });
-  }
+ try {
+  const params = req['body'];
+  const result = await usersModel.comparePassword(params);
+  res.status(200).json(result);
+ } catch (error) {
+  res.status(400).json({ "status": false, "error": [{ "msg": error.msg }] });
+ }
 }
--- models/users/index.js
+++ models/users/index.js
@@ -2,41 +2,47 @@ const mongo = require('../../config/db');
 const hashPassword = require('../../config/password_hashing');
 const jwt = require('../../config/jwt');
 exports.addUser = async (params) => {
-  try {
-    const password = await hashPassword.hashPassword(params['password'], 10)
-    const db = mongo.getDB();
-    const insertObj = {
-      name: params['name'],
-      email: params['email'],
-      password: password,
-    }
-    await db.collection(USERS_COLLECTION).insertOne(insertObj);
-    return;
-  } catch (error) {
-    throw new Error("Error while adding user");
+ try {
+  const password = await hashPassword.hashPassword(params['password'], 10)
+  const db = mongo.getDB();
+  const usersCollection = db.collection(USERS_COLLECTION);
+  const result = await usersCollection.countDocuments({ "name": params['name'], "email": params['email'] })
+  if (result > 0) {
+   return { "status": false, "error": [{ "msg": "Ooops!!! User already exists" }] }
   }
+
+  const insertObj = {
+   name: params['name'],
+   email: params['email'],
+   password: password,
+  }
+  await usersCollection.insertOne(insertObj);
+  return { "status": true, "msg": "Inserted Successfully!!!" };
+ } catch (error) {
+  throw new Error("Error while adding user");
+ }
 }
 exports.getUser = async (params) => {
-  try {
-    const db = mongo.getDB();
-    const result = await db.collection(USERS_COLLECTION).find().toArray();
-    return result;
-  } catch (error) {
-    throw error;
-  }
+ try {
+  const db = mongo.getDB();
+  const result = await db.collection(USERS_COLLECTION).find().toArray();
+  return result;
+ } catch (error) {
+  throw error;
+ }
 }
 exports.comparePassword = async (params) => {
-  try {
-    const db = mongo.getDB();
-    const result = await db.collection(USERS_COLLECTION).find({name: params['name']}).toArray();
-    if(result.length == 1) {
-      const isPasswordCorrect = await hashPassword.comparePassword(result[0]['password'], params['password']);
-      if(isPasswordCorrect) {
-       const token = jwt.generateToken(result[0]);
-       return { "status": isPasswordCorrect, "token": token }
-      } else return { "status": false }
-    } else return { "status": false }
-  } catch (error) {
-    throw new Error("Error while comparing password");
-  }
+ try {
+  const db = mongo.getDB();
+  const result = await db.collection(USERS_COLLECTION).find({ name: params['name'] }).toArray();
+  if (result.length == 1) {
+   const isPasswordCorrect = await hashPassword.comparePassword(result[0]['password'], params['password']);
+   if (isPasswordCorrect) {
+    const token = jwt.generateToken(result[0]);
+    return { "status": isPasswordCorrect, "token": token }
+   } else return { "status": false }
+  } else return { "status": false }
+ } catch (error) {
+  throw new Error("Error while comparing password");
+ }
 }
